20323787ccbd2eb5e8364356cf0b519ece405fc6
Eric Rich <erich@fenrir.redhat.com>
2013-09-24 15:16:28 -0400

    Chaning JNDI endpoints to use Tomcat context.xml

--- src/main/java/MySQLServlet.java
+++ src/main/java/MySQLServlet.java
@@ -3,6 +3,6 @@ package main.java;
 public class MySQLServlet extends BaseServlet {
 
 	public MySQLServlet() {
-		super("jdbc/MysqlDS", "show tables;");
+		super("java:comp/env/jdbc/MysqlDS", "show tables;");
 	}
 }
--- src/main/java/PostgresServlet.java
+++ src/main/java/PostgresServlet.java
@@ -10,6 +10,6 @@ public class PostgresServlet extends BaseServlet {
 	 * 
 	 */
 	public PostgresServlet() {
-		super("jdbc/PostgreSQLDS", "SELECT * FROM pg_catalog.pg_tables");
+		super("java:comp/env/jdbc/PostgreSQLDS", "SELECT * FROM pg_catalog.pg_tables");
 	}
 }
